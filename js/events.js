272b78e9a2297ecc160318479672134e9eba1d1d
MoriSar <morisar.net@gmail.com>
2016-05-12 14:23:18 +0300

    Modified event.js

--- js/events.js
+++ js/events.js
@@ -1,40 +1,42 @@
 "use strict";
 
-var eventObj = {
-	addEvent: function (e, type, fn) {
-		if (typeof addEventListener !== "undefined") {
-			e.addEventListener(type, fn, false);
-		} else if (typeof attachEvent !== "undefined") {
-			e.attachEvent('on' + type, fn);
-		} else {
-			e['on' + type] = fn;
+var eventObj = (function() {
+	return {
+		addEvent: function (e, type, fn) {
+			if (typeof addEventListener !== "undefined") {
+				e.addEventListener(type, fn, false);
+			} else if (typeof attachEvent !== "undefined") {
+				e.attachEvent('on' + type, fn);
+			} else {
+				e['on' + type] = fn;
+			}
+		},
+
+		removeEvent: function (e, type, fn) {
+			if (typeof removeEventListener !== "undefined") {
+				e.removeEventListener(type, fn, false);
+			} else if (typeof attachEvent !== "undefined") {
+				e.detachEvent('on' + type, fn);
+			} else {
+				e['on' + type] = null;
+			}
+		},
+
+		getTarget: function (event) {
+			var event = window.event || event;
+			if (typeof event.target !== "undefined") {
+				return event.target; 
+			} else {
+				return evt.srcElement;
+			};
+		},
+
+		preventDefault: function (event) {
+			if (typeof event.preventDefault !== "undefined") {
+				event.preventDefault();
+			} else {
+				event.returnValue = false;
+			};
 		}
-	},
-
-	removeEvent: function (e, type, fn) {
-		if (typeof removeEventListener !== "undefined") {
-			e.removeEventListener(type, fn, false);
-		} else if (typeof attachEvent !== "undefined") {
-			e.detachEvent('on' + type, fn);
-		} else {
-			e['on' + type] = null;
-		}
-	},
-
-	getTarget: function (event) {
-		var event = window.event || event;
-		if (typeof event.target !== "undefined") {
-			return event.target; 
-		} else {
-			return evt.srcElement;
-		};
-	},
-
-	preventDefault: function (event) {
-		if (typeof event.preventDefault !== "undefined") {
-			event.preventDefault();
-		} else {
-			event.returnValue = false;
-		};
-	}
-};
+	};
+})();
